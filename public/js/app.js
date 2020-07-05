2fb2c2fa6aca96673fd847da65e2b2ac720e7d44
epanks <epanks@gmail.com>
2020-07-06 06:42:46 +0700

    ok

--- public/js/app.js
+++ public/js/app.js
@@ -149,8 +149,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Dev\wire\tall_wave\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Dev\wire\tall_wave\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\code\tall\tall_wave_333\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\code\tall\tall_wave_333\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
